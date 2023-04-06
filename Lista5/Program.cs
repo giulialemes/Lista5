1b860cd61368bedc3d9e05e2b77efa72495e8746
CJ3016927 <CJ3016927@LAB01-PC10>
2023-04-06 10:40:34 -0300

    Adicionar arquivos de projeto.

---
+++ Lista5/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numere;
+            Console.WriteLine("Escolha um exercicio");
+            Console.WriteLine("====== MENU ======");
+            Console.WriteLine("1 - exercicio 1 ");
+            Console.WriteLine("2 - exercicio 2 ");
+            Console.WriteLine("3 - exercicio 3 ");
+            Console.WriteLine("4 - exercicio 4 ");
+            Console.WriteLine("5 - exercicio 5 ");
+            Console.WriteLine("6 - exercicio 6 ");
+            Console.WriteLine("7 - exercicio 7 ");
+            Console.WriteLine("================");
+
+            numere = int.Parse(Console.ReadLine());
+            switch (numere)
+            {
+                case 1: 
+
+
+                    int num;
+                    Console.WriteLine("Digite um numero qualquer: ");
+                    num = int.Parse(Console.ReadLine());
+
+                    for (int i = 1; i < num; i++)
+                        Console.WriteLine("valor de i" + i);
+
+                    Console.ReadKey();
+                    break;  
+
+                case 2:
+
+
+                    int numero;
+                    Console.WriteLine("Digite um numero qualquer: ");
+                    numero = int.Parse(Console.ReadLine());
+
+                    for (int i = 2; i <= numero; i += 2)
+                        Console.WriteLine("valor de i" + i);
+
+
+                    Console.ReadKey();
+                    break;
+
+                case 3:
+
+
+                    int Num;
+                    Console.WriteLine("Digite um numero inteiro: ");
+                    Num = int.Parse(Console.ReadLine());    
+
+                    for (int i = 1000; i > Num; i-= 2)
+                        Console.WriteLine("valor de i" + i );
+
+                    Console.ReadKey ();
+                    break;
+
+
+                case 4:
+
+
+                    int NUMERO, soma; 
+                    
+                    for ( soma = 0; soma <= 200;)
+                    {
+                        Console.WriteLine("Digite um numero: ");
+                        NUMERO = int.Parse(Console.ReadLine());
+
+                        if (NUMERO > 0)
+                            (soma + NUMERO);
+                    }
+
+                    Console.ReadKey(); 
+                    break ;
+
+
+                case 5:
+
+
+                    int number;
+                    Console.WriteLine("Digite um numero: ");
+                    number = int.Parse(Console.ReadLine());
+
+                    for (   
+
+                    Console.ReadKey();
+                    break;
+
+
+                case 6:
+
+
+                    int nume;
+                    Console.WriteLine("Digite 10 numeros: ");
+                    nume = int.Parse(Console.ReadLine());   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+              
+
+           
+
+
+
+            
+
+        }
+    }
+}
